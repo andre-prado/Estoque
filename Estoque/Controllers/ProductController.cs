7629756c026084498e603488bbddae1d17ecf627
andre-prado <andr3prad0@gmail.com>
2021-10-30 22:43:32 -0300

    Cria controller para Estoque

---
+++ Estoque/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Estoque.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estoque.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>();
+
+        [HttpPost]
+        public Product CreateProduct([FromBody]Product product)
+        {
+            products.Add(product);
+            return product;
+
+        }
+    }
+}
