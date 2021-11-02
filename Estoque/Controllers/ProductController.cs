8e88aaf340f88b29ac5bf4dbe9e97fa3ea42e2b1
andre-prado <andr3prad0@gmail.com>
2021-11-02 20:16:01 -0300

    Cria CRUD para produtos - ainda sem banco de dados

--- Estoque/Controllers/ProductController.cs
+++ Estoque/Controllers/ProductController.cs
@@ -9,16 +9,61 @@ namespace Estoque.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ProductController : Controller
+    public class ProductController : ControllerBase
     {
-        List<Product> products = new List<Product>();
+        private static List<Product> products = new List<Product>();
+        private static int id = 1;
 
         [HttpPost]
-        public Product CreateProduct([FromBody]Product product)
+        public List<Product> CreateProduct([FromBody] Product product)
         {
+            product.Id = id++;
             products.Add(product);
-            return product;
+            return products;
+        }
 
+
+        [HttpGet]
+        public List<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProductById([FromRoute] int id)
+        {
+            return products.FirstOrDefault(product => product.Id == id);
+        }
+
+        [HttpPut("{id}")]
+        public Product UpdateProduct([FromBody] Product productUpdated, int id)
+        {
+            Product product = this.GetProductById(id);
+            if(product != null)
+            {
+                product.Description = productUpdated.Description;
+                product.Name = productUpdated.Name;
+                product.Price = productUpdated.Price;
+                product.Quantity = productUpdated.Quantity;
+                return product;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public Boolean DeleteProductById([FromRoute]int id)
+        {
+            Product product = this.GetProductById(id);
+            if(product != null)
+            {
+                products.Remove(product);
+                return true;
+            }
+
+            return false;
         }
     }
 }
